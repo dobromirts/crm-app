2b571cf8571529911194ac0b5553d01757bd9bd5
dobromirts <46093564+dobromirts@users.noreply.github.com>
2025-12-05 10:15:35 +0200

    CRM App (6) initial version

---
+++ src/app/static-data/financial.ts
@@ -0,0 +1,26 @@
+export const Financial: Record<string, any> = {
+  'SalesType': [
+    { "COGS": 0, "Country": "Cell 0.2", "Date": "Cell 0.3", "Discounts": 0, "GrossSales": 0, "ManufacturingPrice": 0, "MonthName": "Cell 0.7", "Product": "Cell 0.8", "Profit": 0, "SalePrice": 0, "Sales": 0, "UnitsSold": "Cell 0.12", "Year": "Cell 0.13", "undefined": "Cell 0.14" },
+    { "COGS": 1, "Country": "Cell 1.2", "Date": "Cell 1.3", "Discounts": 1, "GrossSales": 1, "ManufacturingPrice": 1, "MonthName": "Cell 1.7", "Product": "Cell 1.8", "Profit": 1, "SalePrice": 1, "Sales": 1, "UnitsSold": "Cell 1.12", "Year": "Cell 1.13", "undefined": "Cell 1.14" },
+    { "COGS": 2, "Country": "Cell 2.2", "Date": "Cell 2.3", "Discounts": 2, "GrossSales": 2, "ManufacturingPrice": 2, "MonthName": "Cell 2.7", "Product": "Cell 2.8", "Profit": 2, "SalePrice": 2, "Sales": 2, "UnitsSold": "Cell 2.12", "Year": "Cell 2.13", "undefined": "Cell 2.14" },
+    { "COGS": 3, "Country": "Cell 3.2", "Date": "Cell 3.3", "Discounts": 3, "GrossSales": 3, "ManufacturingPrice": 3, "MonthName": "Cell 3.7", "Product": "Cell 3.8", "Profit": 3, "SalePrice": 3, "Sales": 3, "UnitsSold": "Cell 3.12", "Year": "Cell 3.13", "undefined": "Cell 3.14" },
+    { "COGS": 4, "Country": "Cell 4.2", "Date": "Cell 4.3", "Discounts": 4, "GrossSales": 4, "ManufacturingPrice": 4, "MonthName": "Cell 4.7", "Product": "Cell 4.8", "Profit": 4, "SalePrice": 4, "Sales": 4, "UnitsSold": "Cell 4.12", "Year": "Cell 4.13", "undefined": "Cell 4.14" },
+    { "COGS": 5, "Country": "Cell 5.2", "Date": "Cell 5.3", "Discounts": 5, "GrossSales": 5, "ManufacturingPrice": 5, "MonthName": "Cell 5.7", "Product": "Cell 5.8", "Profit": 5, "SalePrice": 5, "Sales": 5, "UnitsSold": "Cell 5.12", "Year": "Cell 5.13", "undefined": "Cell 5.14" },
+    { "COGS": 6, "Country": "Cell 6.2", "Date": "Cell 6.3", "Discounts": 6, "GrossSales": 6, "ManufacturingPrice": 6, "MonthName": "Cell 6.7", "Product": "Cell 6.8", "Profit": 6, "SalePrice": 6, "Sales": 6, "UnitsSold": "Cell 6.12", "Year": "Cell 6.13", "undefined": "Cell 6.14" },
+    { "COGS": 7, "Country": "Cell 7.2", "Date": "Cell 7.3", "Discounts": 7, "GrossSales": 7, "ManufacturingPrice": 7, "MonthName": "Cell 7.7", "Product": "Cell 7.8", "Profit": 7, "SalePrice": 7, "Sales": 7, "UnitsSold": "Cell 7.12", "Year": "Cell 7.13", "undefined": "Cell 7.14" },
+    { "COGS": 8, "Country": "Cell 8.2", "Date": "Cell 8.3", "Discounts": 8, "GrossSales": 8, "ManufacturingPrice": 8, "MonthName": "Cell 8.7", "Product": "Cell 8.8", "Profit": 8, "SalePrice": 8, "Sales": 8, "UnitsSold": "Cell 8.12", "Year": "Cell 8.13", "undefined": "Cell 8.14" },
+    { "COGS": 9, "Country": "Cell 9.2", "Date": "Cell 9.3", "Discounts": 9, "GrossSales": 9, "ManufacturingPrice": 9, "MonthName": "Cell 9.7", "Product": "Cell 9.8", "Profit": 9, "SalePrice": 9, "Sales": 9, "UnitsSold": "Cell 9.12", "Year": "Cell 9.13", "undefined": "Cell 9.14" }
+  ],
+  'BoxOfficeRevenueType': [
+    { "Franchise": "Cell 0.1", "TotalWorldBoxOfficeRevenue": 0, "HighestGrossingMovieInSeries": 0 },
+    { "Franchise": "Cell 1.1", "TotalWorldBoxOfficeRevenue": 1, "HighestGrossingMovieInSeries": 1 },
+    { "Franchise": "Cell 2.1", "TotalWorldBoxOfficeRevenue": 2, "HighestGrossingMovieInSeries": 2 },
+    { "Franchise": "Cell 3.1", "TotalWorldBoxOfficeRevenue": 3, "HighestGrossingMovieInSeries": 3 },
+    { "Franchise": "Cell 4.1", "TotalWorldBoxOfficeRevenue": 4, "HighestGrossingMovieInSeries": 4 },
+    { "Franchise": "Cell 5.1", "TotalWorldBoxOfficeRevenue": 5, "HighestGrossingMovieInSeries": 5 },
+    { "Franchise": "Cell 6.1", "TotalWorldBoxOfficeRevenue": 6, "HighestGrossingMovieInSeries": 6 },
+    { "Franchise": "Cell 7.1", "TotalWorldBoxOfficeRevenue": 7, "HighestGrossingMovieInSeries": 7 },
+    { "Franchise": "Cell 8.1", "TotalWorldBoxOfficeRevenue": 8, "HighestGrossingMovieInSeries": 8 },
+    { "Franchise": "Cell 9.1", "TotalWorldBoxOfficeRevenue": 9, "HighestGrossingMovieInSeries": 9 }
+  ],
+};
---
+++ src/app/tasks/tasks.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { IGX_GRID_DIRECTIVES } from 'igniteui-angular';
+import { TasksComponent } from './tasks.component';
+
+describe('TasksComponent', () => {
+  let component: TasksComponent;
+  let fixture: ComponentFixture<TasksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TasksComponent, NoopAnimationsModule, FormsModule, ReactiveFormsModule, HttpClientTestingModule, IGX_GRID_DIRECTIVES]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TasksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
